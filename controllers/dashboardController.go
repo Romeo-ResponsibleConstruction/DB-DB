09e49a5b4a9873825a2ac6a1559b52dd68ad5100
EmilyBoarer <prog@emily.boarer.uk>
2023-01-24 15:12:39 +0000

    more useful info displayed now

--- controllers/dashboardController.go
+++ controllers/dashboardController.go
@@ -3,5 +3,8 @@ package controllers
 import "github.com/gofiber/fiber/v2"
 
 func GetDashboard(c *fiber.Ctx) error {
-	return c.JSON(c.App().Stack())
+	return c.JSON(fiber.Map{
+		"hello":  "world",
+		"server": c.Hostname(),
+	})
 }
