195e85280b9b69dde0c3f1c1031da47c48e4d75f
EmilyBoarer <prog@emily.boarer.uk>
2023-02-07 11:41:50 +0000

    started post request inputs

--- controllers/dashboardController.go
+++ controllers/dashboardController.go
@@ -4,6 +4,7 @@ import (
 	"DB-DB/database"
 	"DB-DB/models"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm/clause"
 )
 
 func GetDashboard(c *fiber.Ctx) error {
@@ -12,4 +13,28 @@ func GetDashboard(c *fiber.Ctx) error {
 	database.DB.Find(&tickets)
 
 	return c.JSON(tickets)
+}
+
+func AddTicket(c *fiber.Ctx) error {
+	// Add a new ticket entry to the database
+	// format of ticket not finalised, format of request is draft final
+
+	// TODO parse weight
+	weight := 33 // test value
+
+	// add ticket
+	ticket := models.DeliveryTicket{Id: 2, Weight: uint(weight)}
+
+	result := database.DB.Clauses(clause.OnConflict{DoNothing: true}).Create(&ticket)
+	// nothing affected => conflict => show error
+	if result.RowsAffected == 0 {
+		c.Status(fiber.StatusForbidden)
+		return c.JSON(fiber.Map{
+			"message": "could not add ticket",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -7,4 +7,5 @@ import (
 
 func Setup(app *fiber.App) {
 	app.Get("/", controllers.GetDashboard)
+	app.Post("/newTicket", controllers.AddTicket)
 }
