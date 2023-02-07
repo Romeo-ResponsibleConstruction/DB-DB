936c6fc26978bd9c9473f8f558791dec6f23a4b4
EmilyBoarer <prog@emily.boarer.uk>
2023-02-07 11:46:24 +0000

    primative post inputs

--- controllers/dashboardController.go
+++ controllers/dashboardController.go
@@ -19,12 +19,20 @@ func AddTicket(c *fiber.Ctx) error {
 	// Add a new ticket entry to the database
 	// format of ticket not finalised, format of request is draft final
 
-	// TODO parse weight
-	weight := 33 // test value
+	c.Accepts("application/json")
 
-	// add ticket
-	ticket := models.DeliveryTicket{Id: 2, Weight: uint(weight)}
+	// TODO assign id programatically, rather than requiring it in the json input!
 
+	// parse json
+	ticket := new(models.DeliveryTicket)
+	if err := c.BodyParser(&ticket); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	// store ticket to database
 	result := database.DB.Clauses(clause.OnConflict{DoNothing: true}).Create(&ticket)
 	// nothing affected => conflict => show error
 	if result.RowsAffected == 0 {
