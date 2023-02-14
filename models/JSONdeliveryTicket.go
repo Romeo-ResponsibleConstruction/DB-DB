edec744608efb1584a0551b0831ef8d922573572
MartinRahe <martin.r.rahe@gmail.com>
2023-02-14 12:36:27 +0000

    finalised a format for the input json received from the ocr system

--- models/JSONdeliveryTicket.go
+++ models/JSONdeliveryTicket.go
@@ -1,6 +1,29 @@
 package models
 
-type JSONDeliveryTicket struct {
-	Weight float32 `json:"weight"` // Total weight of the delivery
-	// Duplicate of DeliveryTicket, but with id removed, because that doesn't want to be supplied by the OCR stage
+type JSONDeliveryTicket struct { // Information sent as a post request from the ocr stage
+	Success          bool                 `json:"success"`
+	Weight           JSONValueInformation `json:"weight"` // Total weight of the delivery
+	ImageUrl         string               `json:"image_url"`
+	ErrorInformation JSONErrorInformation `json:"error"`
+}
+
+type JSONValueInformation struct {
+	Value  float32               `json:"value"`
+	Checks JSONChecksInformation `json:"checks"`
+}
+
+type JSONChecksInformation struct {
+	ExtremeValueCheck bool `json:"extremeValueCheck"`
+	DecimalPlaceCheck bool `json:"decimalPlaceCheck"`
+}
+
+type JSONErrorInformation struct {
+	Type        string                     `json:"type"` //field name
+	Description string                     `json:"description"`
+	Likelihoods JSONLikelihoodsInformation `json:"likelihoods"`
+}
+
+type JSONLikelihoodsInformation struct {
+	Threshold float32 `json:"threshold"`
+	Actual    float32 `json:"actual"`
 }
